7d20b76ffe9bcc81987cfd7695f168d86471a2e0
mustaque508 <69106706+mustaque508@users.noreply.github.com>
2021-12-24 12:32:24 +0530

    change server.js file

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,25 +1,25 @@
 /***************************project run from this file****************/
-require('./config/database');
+// require('./config/database');
 const express = require('express');
 const path = require('path');  
 const app=express();
 const PORT=process.env.PORT || 5000;
 app.use(express.json());
 
-// required request[controller] to perform requestForm  operation
-app.use(require('./controllers/request'));
+// // required request[controller] to perform requestForm  operation
+// app.use(require('./controllers/request'));
 
-// required request[controller] to perform purchase  operation
-app.use(require('./controllers/purchase'));
+// // required request[controller] to perform purchase  operation
+// app.use(require('./controllers/purchase'));
 
-// required request[controller] to perform distribute  operation
-app.use(require('./controllers/distribute'));
+// // required request[controller] to perform distribute  operation
+// app.use(require('./controllers/distribute'));
 
-// required request[controller] to perform registerForm  operation
-app.use(require('./controllers/register'));
+// // required request[controller] to perform registerForm  operation
+// app.use(require('./controllers/register'));
 
-// required request[controller] to perform login  operation
-app.use(require('./controllers/login'));
+// // required request[controller] to perform login  operation
+// app.use(require('./controllers/login'));
 
 // used for proxy url
 app.use(express.static(path.join(__dirname,'..','frontEnd','build')));
